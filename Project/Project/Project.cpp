29e83b5adff6ef1bae391902432fc7f371609043
aumkumar parmar <aparmar2250@conestogac.on.ca>
2024-08-11 12:28:08 -0400

    Modify header files and decleare struct for tree and hash table.

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,3 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
+#include <ctype.h>
+
+#pragma warning(disable:4996)
+
+#define HASH_TABLE_SIZE 127
+#define MAX_COUNTRY_NAME 21
+
+typedef struct Parcel
+{
+    char country[MAX_COUNTRY_NAME];
+    int weight;
+    float valuation;
+    struct Parcel* left, * right;
+} Parcel;
+
+typedef struct
+{
+    Parcel* root;
+} HashTable;
